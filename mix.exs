6a0c6ec764dd0dfb8131582f6c34c683dbb0c99f
Richard Livsey <github@livsey.org>
2017-05-31 22:31:30 +0100

    Bump to Apache Tika 1.15

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule Mix.Tasks.Compile.Tika do
     fetch_one(
       "tika-#{version}.jar",
       "http://www-us.apache.org/dist/tika/tika-app-#{version}.jar",
-      "403847bf7ac6f55412949e32c5bc91faca57b1d683d191ee9ccb8d06623a2ef6"
+      "8309d54b7090588f9d1291ce6003d18e7e32e81d5ac1c655553a48228f209ec2"
     )
 
     Mix.shell.info("Done!")
